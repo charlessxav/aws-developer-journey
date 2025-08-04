ab6c99d7a1b8e0696444df4c7a788010004586fc
charlessxav <charlessxav@gmail.com>
2025-08-04 19:12:06 -0400

    Add Day 1 Python basics script

---
+++ aws-learning/week1-python-basics/day1_intro.py
@@ -0,0 +1,11 @@
+name = "Charles"
+favorite_service = "S3"
+certifications = 1
+learning_aws = True
+
+print(f"My name is {name}. I like AWS {favorite_service}. I have {certifications} certifications. Learning AWS: {learning_aws}")
+
+print(type(name))
+print(type(favorite_service))
+print(type(certifications))
+print(type(learning_aws))
