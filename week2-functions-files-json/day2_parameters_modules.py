895b0b6aeb1e0c61abeebe7307ba22b3130ac46c
charlessxav <charlessxav@gmail.com>
2025-08-06 18:32:11 -0400

    feat: add week 2 day 2 - parameterized functions and modules

---
+++ week2-functions-files-json/day2_parameters_modules.py
@@ -0,0 +1,6 @@
+from greetings_module import greet_user
+
+message = greet_user(certs=5, name="Charles")
+print(message)
+
+# Output: Hello Charles, you have 2 AWS certifications!
---
+++ week2-functions-files-json/greetings_module.py
@@ -0,0 +1,2 @@
+def greet_user(name, certs=0):
+    return f"Hello {name}, you have {certs} AWS certifications!"
